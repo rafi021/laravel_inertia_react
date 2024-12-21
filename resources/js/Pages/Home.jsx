bd5392c77bdb2289aaa24e6248c9321bda323615
rafi021 <mahmud.ibrahim021@gmail.com>
2024-12-21 08:39:38 +0600

    scroll position

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,8 +1,14 @@
+import { Link } from '@inertiajs/react'
 import React from 'react'
 
 const Home = ({ name }) => {
     return (
-        <div className='title'>Hello {name} </div>
+        <>
+            <div className='title'>Hello {name} </div>
+            <Link preserveScroll href="/" className='block mt-[1000px] title'>
+                {new Date().toLocaleTimeString()}
+            </Link>
+        </>
     )
 }
 
